d30ce3d0ab8f45d2704151ca621e97b826c7ae5a
Scott Chamberlain <myrmecocystus@gmail.com>
2016-04-12 14:25:14 -0700

    first commit

---
+++ R/bound_box.R
@@ -0,0 +1,11 @@
+#' Bounding box
+#'
+#' @export
+#' @param x input
+#' @examples
+#' library("lawn")
+#' library("geojson")
+#' bound_box(as.geojson(lawn_data$polygons_count))
+bound_box <- function(x) {
+  lawn::lawn_extent(x)
+}
---
+++ R/geoops-package.R
@@ -0,0 +1,11 @@
+#' geoops
+#'
+#' @import geojson
+#' @importFrom jqr jq
+#' @importFrom lawn lawn_within
+#' @importFrom lazyeval all_dots lazy_dots
+#' @name geoops-package
+#' @aliases geoops
+#' @docType package
+#' @keywords package
+NULL
