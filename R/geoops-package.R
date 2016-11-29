75347abb1a01a661eb90a957b631d716c27533a6
Scott Chamberlain <myrmecocystus@gmail.com>
2016-11-28 22:16:55 -0800

    remove sifter stuff, remove jqr and lawn from deps

--- R/geoops-package.R
+++ R/geoops-package.R
@@ -3,9 +3,6 @@
 #' @useDynLib geoops
 #' @importFrom Rcpp sourceCpp
 #' @import geojson
-#' @importFrom jqr jq
-#' @importFrom lawn lawn_within
-#' @importFrom lazyeval all_dots lazy_dots
 #' @name geoops-package
 #' @aliases geoops
 #' @docType package
