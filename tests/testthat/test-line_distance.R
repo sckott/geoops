7bfb48aa2f885616e2fb6888302b570097d00652
Scott Chamberlain <myrmecocystus@gmail.com>
2017-02-20 17:38:45 -0800

    fix line_distance test for real

--- tests/testthat/test-line_distance.R
+++ tests/testthat/test-line_distance.R
@@ -4,7 +4,7 @@ line <- '{
   "type": "Feature",
   "properties": {},
   "geometry": {
-    "type": "Linestring",
+    "type": "LineString",
     "coordinates": [
       [-77.031669, 38.878605],
       [-77.029609, 38.881946],
@@ -25,9 +25,25 @@ test_that("destination works well", {
   expect_lte(geo_line_distance(line, 'radians'), 0.001)
 })
 
+badline <- '{
+  "type": "Feature",
+  "properties": {},
+  "geometry": {
+    "type": "LineString",
+    "coordinates": [
+      [-77.031669, 38.878605],
+      [-77.029609, 38.881946],
+      [-77.020339, 38.884084],
+      [-77.025661, 38.885821],
+      [-77.021884, 38.889563],
+      [-77.019824, 38.892368]
+    ]
+  }
+}'
+
 test_that("line_distance fails well", {
   expect_error(
-    geo_line_distance(line),
+    geo_line_distance(badline),
     "input must be a LineString, "
   )
 
