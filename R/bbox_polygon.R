9a383646a16b1ef69310d1a10f9e9c98a77a7533
Scott Chamberlain <myrmecocystus@gmail.com>
2017-03-21 10:03:03 -0700

    remove jsonlite usage in eg

--- R/bbox_polygon.R
+++ R/bbox_polygon.R
@@ -6,12 +6,9 @@
 #' @param bbox extent in `[minX, minY, maxX, maxY]` order
 #' @return [Feature]<[Polygon]> a Polygon representation of the bounding box
 #' @examples
-#' z <- geo_bbox_polygon(c(0, 0, 10, 10))
-#' z <- geo_bbox_polygon(c(-90, -30, -70, -10))
-#' z <- geo_bbox_polygon(c(0, 0, 10, 10))
-#'
-#' jsonlite::toJSON(jsonlite::fromJSON(z), pretty=TRUE, auto_unbox = TRUE) %>%
-#'   lawn::view()
+#' geo_bbox_polygon(c(0, 0, 10, 10))
+#' geo_bbox_polygon(c(-90, -30, -70, -10))
+#' geo_bbox_polygon(c(0, 0, 10, 10))
 geo_bbox_polygon <- function(bbox) {
   bbox_polygon(bbox)
 }
