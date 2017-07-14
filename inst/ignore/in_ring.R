bb9781b1f9f21f6694c5de454c2e2ea64ebc4616
Scott Chamberlain <myrmecocystus@gmail.com>
2017-07-14 12:02:20 -0700

    move in_ring to inst/ignore for now - not ready yet - part of inside() fxn

--- inst/ignore/in_ring.R
+++ inst/ignore/in_ring.R
@@ -1,8 +1,9 @@
-#' Get coordinates
+#' Check if a Point is in a GeoJSON Ring
 #'
 #' @export
 #' @param pt a point
 #' @param ring a ring
+#' @return boolean, `FALSE` if point is not in the ring, `TRUE` if it is
 #' @examples \dontrun{
 #' pt <- "[-77, 44]"
 #' ring <- "[[10,59],[10.1,59.8],[10.3,60],[10.3,59],[10,59]]"
